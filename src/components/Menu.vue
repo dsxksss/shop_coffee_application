0c6c60a9eb3e988114b431d807a5b6c32f974e7c
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-02-10 20:08:45 +0800

    优化列表渲染，减少代码重复

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,25 +1,31 @@
 <script setup>
 import { BeakerIcon } from '@heroicons/vue/24/solid'
+import {ref} from "vue";
+const items = ref([
+    {
+        index:1,
+        title:"title 1",
+        component:BeakerIcon,
+    },
+    {
+        index:2,
+        title:"title 2",
+        component:BeakerIcon,
+    },
+    {
+        index:3,
+        title:"title 3",
+        component:BeakerIcon,
+    },
+]);
 </script>
 
 <template>
     <ul class="menu bg-base-200 w-40 rounded-box space-y-4">
-        <li>
+        <li v-for="item in items" :key="item.index">
             <a>
-                <BeakerIcon class="h-5 w-5" />
-                title1
-            </a>
-        </li>
-        <li>
-            <a>
-                <BeakerIcon class="h-5 w-5" />
-                title2
-            </a>
-        </li>
-        <li>
-            <a>
-                <BeakerIcon class="h-5 w-5" />
-                title3
+               <component :is="item.component" class="w-6 h-6"></component>
+                <span>{{ item.title }}</span>
             </a>
         </li>
     </ul>
