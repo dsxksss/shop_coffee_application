b56a474561812a2fe5e22099903e57f03d71fd34
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-02-10 10:29:16 +0800

    修改daisyUI颜色配置

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,7 +13,7 @@ module.exports = {
         mytheme: {
           "primary": "#4b5563",
           "secondary": "#6b7280",
-          "accent": "#2dd4bf",
+          "accent": "#0891b2",
           "neutral": "#374151",
           "base-100": "#1f2937",
           "info": "#0ea5e9",
