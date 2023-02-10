e16e037e3acb7e47be362fa5c4952a12a8702fb4
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-02-10 20:23:21 +0800

    优化样式

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,21 +1,34 @@
 <script setup>
-import { BeakerIcon } from '@heroicons/vue/24/solid'
+import { BuildingStorefrontIcon } from '@heroicons/vue/24/solid'
+import { ShoppingCartIcon } from '@heroicons/vue/24/solid'
+import { UserIcon } from '@heroicons/vue/24/solid'
+import { Cog6ToothIcon } from '@heroicons/vue/24/solid'
+
 import {ref} from "vue";
 const items = ref([
     {
         index:1,
-        title:"title 1",
-        component:BeakerIcon,
+        title:"商品",
+        iconColor:'',
+        component: BuildingStorefrontIcon,
     },
     {
         index:2,
-        title:"title 2",
-        component:BeakerIcon,
+        title:"购物车",
+        iconColor:'',
+        component: ShoppingCartIcon,
     },
     {
         index:3,
-        title:"title 3",
-        component:BeakerIcon,
+        title:"个人",
+        iconColor:'',
+        component: UserIcon,
+    },
+    {
+        index:4,
+        title:"设置",
+        iconColor:'',
+        component: Cog6ToothIcon,
     },
 ]);
 </script>
@@ -24,8 +37,8 @@ const items = ref([
     <ul class="menu bg-base-200 w-40 rounded-box space-y-4">
         <li v-for="item in items" :key="item.index">
             <a>
-               <component :is="item.component" class="w-6 h-6"></component>
-                <span>{{ item.title }}</span>
+               <component :is="item.component" :class="`w-6 h-6 ${item.iconColor}`"></component>
+                <span class="text-md">{{ item.title }}</span>
             </a>
         </li>
     </ul>
