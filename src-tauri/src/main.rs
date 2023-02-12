3ab18086894f4fb9769c3f72dfc55fdbfeae5b6a
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-02-12 09:15:14 +0800

    添加windows label 配置(可根据label名控制window对象)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,19 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn close_window(window: tauri::Window) {
+    if let Some(application_window) = window.get_window("ApplicationWindow") {
+        application_window.close().unwrap();
+    }
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![close_window])
         // .menu(tauri::Menu::os_default(&tauri::generate_context!().package_info().name))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
