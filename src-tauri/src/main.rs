f43beb1286d81adf510a9cbb7607fe0e10956d75
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-02-28 15:41:44 +0800

    删除多余文件 更改tauri配置

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,23 +8,14 @@
 use tauri::Manager;
 use window_shadows::set_shadow;
 
-#[tauri::command]
-async fn close_window(window: tauri::Window) {
-    if let Some(application_window) = window.get_window("ApplicationWindow") {
-        application_window.close().unwrap();
-    }
-}
-
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            let main_window = app.get_window("ApplicationWindow").unwrap();
             // 设置窗口阴影及圆角
-            #[cfg(any(windows, target_os = "windows"))]
-            set_shadow(main_window, true).unwrap();
+            set_shadow(app.get_window("ApplicationWindow").unwrap(), true).unwrap();
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![close_window])
+        // .invoke_handler(tauri::generate_handler![])
         // .menu(tauri::Menu::os_default(&tauri::generate_context!().package_info().name))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
