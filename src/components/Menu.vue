5142100d2b54c47e80de8f3dba09f37723f60e1e
VentCat <2546650292@qq.com>
2023-07-02 12:54:25 +0800

    fix 修复key值指定错误

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -9,7 +9,7 @@ const items = ref(menuItems);
 
 <template>
     <ul class="fixed top-0 h-screen justify-center pl-6 menu rounded-box p-2 space-y-2">
-        <li v-for="item in items" :key="item.index">
+        <li v-for="item in items" :key="item.path">
             <RouterLink :to="item.path" :class="router.currentRoute.value.path == item.path ? 'active' : null">
                 <component :is="item.component" :class="`w-6 h-6 ${item.iconColor}`"></component>
             </RouterLink>
